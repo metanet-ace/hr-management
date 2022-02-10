4d72c752fed7f7364479432a81d2fb0e60ae0ef1
suin0 <suin0@DESKTOP-TQLDIHN>
2022-02-10 11:44:38 +0900

    spring scheduler를 이용한 출석률 업데이트

--- src/main/java/com/metanet/service/suinEduServiceImpl.java
+++ src/main/java/com/metanet/service/suinEduServiceImpl.java
@@ -1,10 +1,20 @@
 package com.metanet.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.metanet.domain.AttendanceVO;
+import com.metanet.domain.EduHistoyVO;
 import com.metanet.domain.EduVO;
 import com.metanet.persistence.suinEduMapper;
+
 
 @Service
 public class suinEduServiceImpl implements suinEduService {
@@ -13,9 +23,32 @@ public class suinEduServiceImpl implements suinEduService {
 	suinEduMapper mapper;
 
 	@Override
-	public int eduAdd(EduVO vo) {
+	public void eduAdd(EduVO vo) {
 		mapper.eduInsert(vo);
-		return 0;
+	}
+
+	@Override
+	public void eduAttendance(EduHistoyVO del) throws ParseException {
+		List<AttendanceVO> list = mapper.eduHistorySelect();
+		Calendar getToday = Calendar.getInstance();
+		getToday.setTime(new Date());
+		
+		for(AttendanceVO vo : list) {
+			Date startD = new SimpleDateFormat("yyyyMMdd").parse(vo.getEduStart());
+			Date endD = new SimpleDateFormat("yyyyMMdd").parse(vo.getEduEnd());
+			
+			Calendar start = Calendar.getInstance();
+			Calendar end = Calendar.getInstance();
+			
+			start.setTime(startD);
+			end.setTime(endD);
+			
+			float diffSecBunja = getToday.getTimeInMillis() - start.getTimeInMillis();
+			float diffSecBunmo = end.getTimeInMillis() - start.getTimeInMillis();
+			
+			vo.setAttendance((diffSecBunja/diffSecBunmo)*100);			
+		}
+		mapper.eduAttendanceUpdate(list);
 	}
 
 }
