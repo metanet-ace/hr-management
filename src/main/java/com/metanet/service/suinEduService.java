6ec59a3d01be64c83545a740d8131532cc5333a9
MoonSuin <official.suin@gmail.com>
2022-02-16 16:27:16 +0900

    교육결과페이지 페이징

--- src/main/java/com/metanet/service/suinEduService.java
+++ src/main/java/com/metanet/service/suinEduService.java
@@ -6,13 +6,15 @@ import java.util.Map;
 
 import com.metanet.domain.EduHistoryVO;
 import com.metanet.domain.EduVO;
+import com.metanet.domain.PageDTO;
 
 public interface suinEduService {
 	public void eduAdd(EduVO vo);
 	public void eduAttendance() throws ParseException;
-	public List<EduHistoryVO> getEduHistoryList();
+	public List<EduHistoryVO> getEduHistoryList(PageDTO dto);
 	public List<EduHistoryVO> getEduHistoryListByKey(Map<String, String> map);
 	public void eduHistoryAdd(Map<String, Object> param);
 	public void eduScoreUpdate(List<EduHistoryVO> list);
 	public void eduProgress() throws ParseException;
+	public int eduHistoryTotalCount(Map<String, String> map);
 }
