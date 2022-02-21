0d638b5269cb6e1d684b5ba99a69f84fa15d854b
DESKTOP-A037TC6\user <koentc010@gmail.com>
2022-02-21 17:46:45 +0900

    버그수정) 스프링시큐리티 재설정

--- src/main/java/com/metanet/config/SecurityConfig.java
+++ src/main/java/com/metanet/config/SecurityConfig.java
@@ -25,11 +25,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity security) throws Exception{
 		security.authorizeRequests()
 				.antMatchers("/signin").permitAll()
-				.antMatchers("/main", "/notice").authenticated()
 				// hasRole은 접두어 "ROLE_"이 디폴트로 붙어 있다.DB에 ROLE_ADMIN, ROLE_USER 등으로 저장했다면 사용가능
 				// 현재는 부서번호로 권한을 주기 때문에 ROLE_이 붙으면 안되므로 hasAuthority 사용 
 				.antMatchers("/admin/**").hasAuthority("인사팀")
-				.antMatchers("/edu/**").hasAuthority("인사팀");
+				.antMatchers("/edu/log", "/edu/detail", "/edu/update", "/edu/delete", 
+						"/edu/add", "/updateNotice",  "/noticeAdd", "/deleteNotice",
+						"/edu/list/**", "/edu/allocation/**", "/edu/score", "/edu/calendar").hasAuthority("인사팀")
+				.antMatchers("/main", "/notice", "/edu/eduEmpDetail","/edu/history", "/edu/download", 
+						"/edu/empEduCalendar", "/noticeDetail").authenticated();
+				
 		
 		security.csrf().disable();
 		// 시큐리티 로그인 페이지를 대체할 커스템 페이지 지정
